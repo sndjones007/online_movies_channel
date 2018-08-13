50d1e185134b95bb367b705780a86a8150f27f7e
sndjones007 <sndjones007@gmail.com>
2018-08-13 11:04:06 +0530

    update

---
+++ back-end/onlinemovieschannel/src/main/java/com/omc/rsb/moviechannelsvc/MoviechannelsvcApplication.java
@@ -0,0 +1,12 @@
+package com.omc.rsb.moviechannelsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviechannelsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviechannelsvcApplication.class, args);
+	}
+}
---
+++ back-end/onlinemovieschannel/src/test/java/com/omc/rsb/moviechannelsvc/MoviechannelsvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.omc.rsb.moviechannelsvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoviechannelsvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
